fb97be7310716973f0b7f7a592c455de715dccdd
inkblotty <inkblot.and.blush@gmail.com>
2016-03-24 12:39:12 -0600

    grabbed query date and responded with json object in server.js

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 var express = require('express');
 var app = express();
 
+var http = require('http').Server(app);
 var path = require('path');
 var port = process.env.PORT || 8080;
 
-// req.query
+var moment = require('moment');
+
+app.get('/:date', function(req, res) {
+	var queryDate = new Date(req.params.date);
+	console.log(queryDate);
+
+	if (queryDate !== 'Invalid Date') {
+		var unixDate = moment.unix(moment(queryDate));
+		var naturalDate = moment(queryDate, "MM DD, YYYY");
+		res.json(
+			{"unix": unixDate, "natural": naturalDate}
+		)
+	}
+	else { res.send('hi'); }
+})
+
+http.listen(port, function() {
+	console.log('listening on *:' + port);
+});
